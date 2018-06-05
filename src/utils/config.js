e9adac6d490a94da7e0cb5dbfa10bc9b9dd45339
yangfei <975694783@qq.com>
2018-06-05 22:47:32 +0800

    add pay icons

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,172 +1,172 @@
 // 账本支出类目
 export const payCateList = [
   {
-    type: 'pay',
+    type: 'catering',
     cateId: 1,
     cateName: '餐饮'
   },
   {
-    type: 'pay',
+    type: 'gouwu',
     cateId: 2,
     cateName: '购物'
   },
   {
-    type: 'pay',
+    type: 'riyong',
     cateId: 3,
     cateName: '日用'
   },
   {
-    type: 'pay',
+    type: 'jiaotongxiaojiaochesijiachemianxing',
     cateId: 4,
     cateName: '交通'
   },
   {
-    type: 'pay',
+    type: 'shucai',
     cateId: 5,
     cateName: '蔬菜'
   },
   {
-    type: 'pay',
+    type: 'shuiguo',
     cateId: 6,
     cateName: '水果'
   },
   {
-    type: 'pay',
+    type: 'lingshi',
     cateId: 7,
     cateName: '零食'
   },
   {
-    type: 'pay',
+    type: 'hfenlei6',
     cateId: 8,
     cateName: '运动'
   },
   {
-    type: 'pay',
+    type: 'yule',
     cateId: 9,
     cateName: '娱乐'
   },
   {
-    type: 'pay',
+    type: 'tongxunfei',
     cateId: 10,
     cateName: '通讯'
   },
   {
-    type: 'pay',
+    type: 'fushi',
     cateId: 11,
     cateName: '服饰'
   },
   {
-    type: 'pay',
+    type: 'iconfontmeirongxihu',
     cateId: 12,
     cateName: '美容'
   },
   {
-    type: 'pay',
+    type: 'gerenzhufanggongjijinchaxun',
     cateId: 13,
     cateName: '住房'
   },
   {
-    type: 'pay',
+    type: 'jujia',
     cateId: 14,
     cateName: '居家'
   },
   {
-    type: 'pay',
+    type: 'wodehaizi',
     cateId: 15,
     cateName: '孩子'
   },
   {
-    type: 'pay',
+    type: 'changbei',
     cateId: 16,
     cateName: '长辈'
   },
   {
-    type: 'pay',
+    type: 'shejiao',
     cateId: 17,
     cateName: '社交'
   },
   {
-    type: 'pay',
+    type: 'lvhang',
     cateId: 18,
     cateName: '旅行'
   },
   {
-    type: 'pay',
+    type: 'yanjiuyan',
     cateId: 19,
     cateName: '烟酒'
   },
   {
-    type: 'pay',
+    type: 'shuma',
     cateId: 20,
     cateName: '数码'
   },
   {
-    type: 'pay',
+    type: 'baoji',
     cateId: 21,
     cateName: '汽车'
   },
   {
-    type: 'pay',
+    type: 'yiliao',
     cateId: 22,
     cateName: '医疗'
   },
   {
-    type: 'pay',
+    type: 'shuji',
     cateId: 23,
     cateName: '书籍'
   },
   {
-    type: 'pay',
+    type: 'xuexi',
     cateId: 24,
     cateName: '学习'
   },
   {
-    type: 'pay',
+    type: 'chongwu',
     cateId: 25,
     cateName: '宠物'
   },
   {
-    type: 'pay',
+    type: 'ankanglijin',
     cateId: 26,
     cateName: '礼金'
   },
   {
-    type: 'pay',
+    type: 'ai-hongbao-wx',
     cateId: 27,
     cateName: '礼物'
   },
   {
-    type: 'pay',
+    type: 'bangong',
     cateId: 28,
     cateName: '办公'
   },
   {
-    type: 'pay',
+    type: 'weixiu',
     cateId: 29,
     cateName: '维修'
   },
   {
-    type: 'pay',
+    type: 'juanzeng',
     cateId: 30,
     cateName: '捐赠'
   },
   {
-    type: 'pay',
+    type: 'caipiao',
     cateId: 31,
     cateName: '彩票'
   },
   {
-    type: 'pay',
+    type: 'qinyoushezhi',
     cateId: 32,
     cateName: '亲友'
   },
   {
-    type: 'pay',
+    type: 'kuaidi',
     cateId: 33,
     cateName: '快递'
   },
   {
-    type: 'pay',
+    type: 'qita',
     cateId: 34,
     cateName: '其他'
   }
@@ -175,32 +175,32 @@ export const payCateList = [
 // 账本收入类目
 export const incomeCateList = [
   {
-    type: 'income',
+    type: 'gongzi',
     cateId: 1,
     cateName: '工资'
   },
   {
-    type: 'income',
+    type: 'jianzhi',
     cateId: 2,
     cateName: '兼职'
   },
   {
-    type: 'income',
+    type: 'licai',
     cateId: 3,
     cateName: '理财'
   },
   {
-    type: 'income',
+    type: 'ankanglijin',
     cateId: 4,
     cateName: '礼金'
   },
   {
-    type: 'income',
+    type: 'ai-hongbao-wx',
     cateId: 5,
     cateName: '红包'
   },
   {
-    type: 'income',
+    type: 'qita',
     cateId: 6,
     cateName: '其他'
   }
